062dc7a4cb326602b901b32b1bace77c5f3c9898
jordanbacon <jmbacon92@gmail.com>
2019-08-22 13:59:17 -0700

    added CRUD methods

--- app/controllers/api/v1/movies_controller.rb
+++ app/controllers/api/v1/movies_controller.rb
@@ -10,4 +10,30 @@ class Api::V1::MoviesController < ApplicationController
         render json: @movie, only: [:id, :title], include: :questions
     end
 
+    def create
+        movie = Movie.create(movie_params)
+        render json: movie
+      end
+    
+    def update
+        @movie.update(movie_params)
+        render json: @movie
+    end
+    
+    def destroy
+        movieId = @movie.id
+        @movie.destroy
+        render json: {message:"Movie deleted", movieId:movieId}
+    end
+  
+    private
+    
+    def movie_params
+        params.permit(:title, :username, :score)
+    end
+    
+    def set_params
+        @movie = Movie.find(params[:id])
+    end  
+
 end
--- app/controllers/api/v1/questions_controller.rb
+++ app/controllers/api/v1/questions_controller.rb
@@ -5,4 +5,33 @@ class Api::V1::QuestionsController < ApplicationController
         render json: @questions, only: [:id, :description, :possible_answer, :correct_answer, :movie_id], include: :movie
     end
 
+    def show
+        render json: @question
+    end
+    
+    def create
+        question = Question.create(question_params)
+        render json: question
+    end
+    
+    def update
+        @question.update(question_params)
+        render json: @question
+    end
+    
+    def destroy
+        questionId = @question.id
+        @question.destroy
+        render json: {message:"Question deleted", questionId:questionId}
+    end
+    
+    private
+    def question_params
+        params.permit(:movie_id, :description, :possible_answer1, :possible_answer2, :possible_answer3, :possible_answer4, :correct_answer)
+    end
+    
+    def set_question
+        @question = Question.find(params[:id])
+    end  
+
 end
